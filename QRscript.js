e3f210448478cdb60ca6199862b5ef6a0fe6fe66
NiaDSN <nia@tain.ai>
2025-03-24 17:08:53 +0900

    Add files via upload

---
+++ QRscript.js
@@ -0,0 +1,19 @@
+// 기기를 감지하고 리디렉션을 처리하는 함수
+window.onload = function() {
+    var userAgent = navigator.userAgent.toLowerCase();
+    
+    // Android 기기 감지
+    if (userAgent.indexOf("android") > -1) {
+        // 구글 플레이스토어로 리디렉션
+        window.location.href = "https://play.google.com/store/apps/details?id=com.yourappname";
+    }
+    // iOS 기기 감지
+    else if (userAgent.indexOf("iphone") > -1 || userAgent.indexOf("ipod") > -1 || userAgent.indexOf("ipad") > -1) {
+        // 앱스토어로 리디렉션
+        window.location.href = "https://apps.apple.com/us/app/yourappname/id123456789";
+    }
+    // 기타 디바이스 감지 (웹 브라우저 등)
+    else {
+        document.body.innerHTML = "<h1>이 디바이스는 지원되지 않습니다.</h1>";
+    }
+};
